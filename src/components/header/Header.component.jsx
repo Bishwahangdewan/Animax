5080a6da84a8292bbb71e85a74389a3c1fae949b
Bishwahang Dewan <bishwahangdewan123@gmail.com>
2021-12-20 17:21:19 +0530

    Header created

---
+++ src/components/header/Header.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+//logo import
+import Logo from '../../assets/logo.png';
+
+//react-router-components import
+import { Link } from 'react-router-dom';
+
+import './header.styles.scss';
+
+const Header = () => (
+    <div className="header">
+        <div>
+            {/*-----LOGO------*/}
+
+            <Link to='/'>
+                <img className="logo" src={Logo} alt="logo" />
+            </Link>
+
+            {/*-------NAV LINKS--------*/}
+
+            <nav>
+                <Link className="nav-link" to="/">Sign In</Link>
+                <Link className="nav-link" to="/">Sign Up</Link>
+            </nav>
+        </div>
+    </div>
+)
+
+export default Header
