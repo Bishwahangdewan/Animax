5714018c245d64b1296c5b459668b3ddd99d0d32
Bishwahang Dewan <bishwahangdewan123@gmail.com>
2021-12-26 14:02:51 +0530

    Build: Responsive Landing Page

--- src/components/custom-button/CustomButton.component.jsx
+++ src/components/custom-button/CustomButton.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './CustomButton.styles.scss';
 
+import { Link } from 'react-router-dom';
+
 const CustomButton = (props) => (
-    <button className={`${props.primary ? 'primary-btn' : ''} btn`}>{props.children}</button>
+    <Link to="/" className={`${props.primary ? 'primary-btn' : ''} btn`}>{props.children}</Link>
 )
 
 export default CustomButton;
